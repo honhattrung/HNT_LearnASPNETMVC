27f9c44b859d8757d2d68579488c8852fe83850f
trung <quanglauang@gmail.com>
2019-04-18 15:48:05 +0700

    update

--- WebApp/WebApp.Tests/Controllers/ExpenditureControllerTest.cs
+++ WebApp/WebApp.Tests/Controllers/ExpenditureControllerTest.cs
@@ -26,5 +26,25 @@ namespace WebApp.Tests.Controllers {
             Assert.IsNotNull(model);
             Assert.AreEqual(db.Expenditures.Count(),model.Count);
         }
+        [TestMethod]
+        public void TestCreateG() {
+            var controller = new ExpenditureController();
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void TestEditG() {
+            var controller = new ExpenditureController();
+            var result0 = controller.Edit(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+
+            var db = new QUANLYCHITIEUEntities();
+            var item = db.Expenditures.First();
+            var result1 = controller.Edit(item.ID) as ViewResult;
+            Assert.IsNotNull(result1);
+            var model = result1.Model as Expenditure;
+            Assert.AreEqual(item.ID, model.ID);
+
+        }
     }
 }
