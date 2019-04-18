11c4a0e90d5a1eb9353dbd8bc89085a023a12353
trung <quanglauang@gmail.com>
2019-04-18 15:36:44 +0700

    update

---
+++ DemoWeb/DemoWeb/Controllers/ArrayListController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class ArrayListController : Controller
+    {
+        static object[] Buffer = new object[10];
+        static int Length = 0;
+        public ActionResult Index()
+        {
+            var model= new Tuple<object[], int>(Buffer, Length);
+
+            return View(model);
+        }
+        public ActionResult Append(string value) {
+            Buffer[Length] = value;
+            Length++;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Clear() {
+            Length = 0;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Insert(int key, string value) {
+            for (int i = Length-1; i>= key; i--) {
+                Buffer[i + 1] = Buffer[i];
+                Buffer[key] = value;
+                Length++;
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+	}
+}
---
+++ WebApp/WebApp.Tests/Controllers/ExpenditureControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Models;
+using WebApp.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+
+using WebApp;
+
+
+namespace WebApp.Tests.Controllers {
+    [TestClass]
+    public class ExpenditureControllerTest {
+        [TestMethod]
+        public void TestIndex() {
+            var db = new QUANLYCHITIEUEntities();
+            var controller = new ExpenditureController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            var model = result.Model as List<Expenditure>;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(db.Expenditures.Count(),model.Count);
+        }
+    }
+}
